4e1b79cd146a979bcf2ce32bc4a6484e09ee36b6
0713jake <0713jake@naver.com>
2026-03-23 13:38:21 +0900

    core - 404 에러 및 일반 에러 핸들러 추가

--- src/main/java/com/samsung/azasuguesthouse/common/auth/SessionCheckInterceptor.java
+++ src/main/java/com/samsung/azasuguesthouse/common/auth/SessionCheckInterceptor.java
@@ -5,12 +5,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
 @Component
 public class SessionCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (handler instanceof ResourceHttpRequestHandler) {
+            return true;
+        }
+
         HttpSession session = request.getSession(false);
 
         if (session == null || session.getAttribute("member") == null) {
--- src/main/java/com/samsung/azasuguesthouse/common/response/ExceptionResponseHandler.java
+++ src/main/java/com/samsung/azasuguesthouse/common/response/ExceptionResponseHandler.java
@@ -6,14 +6,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class ExceptionResponseHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    protected ResponseEntity<ExceptionResponse> handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(HttpServletResponse.SC_NOT_FOUND, "Not Found"));
+    }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ExceptionResponse> handleUnauthorizedException(UnauthorizedException e) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new ExceptionResponse(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(HttpServletResponse.SC_BAD_REQUEST, e.getClass().getName()));
     }
 }
