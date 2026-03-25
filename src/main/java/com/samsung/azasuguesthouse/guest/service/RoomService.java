cf18fd93f07f7bb5e36da834d1fab953f4a06962
jingeon <sjg729729@gmail.com>
2026-03-25 11:29:20 +0900

    [Guest] - Room Cache 적용

--- src/main/java/com/samsung/azasuguesthouse/guest/service/RoomService.java
+++ src/main/java/com/samsung/azasuguesthouse/guest/service/RoomService.java
@@ -22,47 +22,34 @@ public class RoomService {
         this.roomCache = roomCache;
     }
 
+    // 1. roomCache에서 가져와야 한다.
+    // 2. roomCache를 업데이트 하는 것은 roomCache 객체의 역할이다.
+    // 3. roomCache.get()을 사용하고 reload는 roomCache 내부에서 관리한다.
+    // 4. roomCache가 변경되는 것은 관리자가 객실 정보를 수정할 때이다.
+    // 5. 날짜, 인원 수 조건 검색은 reservationCache, roomCache를 활용한다.
+
+
     public RoomDto getRoomById(long roomId) {
 
-        Room room = roomMapper.findById(roomId);
+//        Room room = roomMapper.findById(roomId);
+//
+//        return new RoomDto(
+//                room.getId(),
+//                room.getRoomName(),
+//                room.getPrice(),
+//                room.getCapacity(),
+//                room.getPicture(),
+//                room.getDescription(),
+//                room.getPolicy()
+//        );
 
-        return new RoomDto(
-                room.getId(),
-                room.getRoomName(),
-                room.getPrice(),
-                room.getCapacity(),
-                room.getPicture(),
-                room.getDescription(),
-                room.getPolicy()
-        );
+        return roomCache.get(roomId);
 
     }
+
 
     public List<RoomDto> getAllRooms() {
-        if (roomCache.isEmpty()) {
-            System.out.println("객실 캐시가 비어있습니다. DB에서 전체 데이터를 로드합니다.");
-            loadAllRoomsToCache();
-        }
-
-        roomCache.printAll();
 
         return roomCache.getAll();
-    }
-
-    private synchronized void loadAllRoomsToCache() {
-        // 다시 한 번 비어있는지 확인 (멀티쓰레드 방어)
-        if (roomCache.isEmpty()) {
-            // DB에서 모든 객실 정보를 가져옴
-            List<RoomDto> rooms = roomMapper.findAllRooms();
-
-            // List를 Map<Long, RoomDto> 구조로 변환
-            Map<Long, RoomDto> allData = rooms.stream()
-                    .collect(Collectors.toMap(
-                            RoomDto::getRoomId, // Key: 객실 ID
-                            room -> room    // Value: 객실 객체
-                    ));
-
-            roomCache.putAll(allData);
-        }
     }
 }
