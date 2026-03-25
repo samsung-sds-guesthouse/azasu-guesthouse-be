2e909e5dedab714739c72a3b883b251f4fc866b0
0713jake <0713jake@naver.com>
2026-03-25 13:42:54 +0900

    core - admin 권한 체크를 우선하도록 하여, 정당한 권원이 없는 사용자에게 401이 아닌 404가 반환되도록 수정 ; rooms api의 경우 세션 검증을 하지 않도록 수정 ; 불필요하게 세션 정보를 attribute에 추가로 넣지 않도록 수정

--- src/main/java/com/samsung/azasuguesthouse/common/log/RequestLogInterceptor.java
+++ src/main/java/com/samsung/azasuguesthouse/common/log/RequestLogInterceptor.java
@@ -3,6 +3,7 @@ package com.samsung.azasuguesthouse.common.log;
 import com.samsung.azasuguesthouse.entity.member.Member;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,19 +15,28 @@ public class RequestLogInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         long memberId = 0;
-        if (request.getAttribute("member") != null && request.getAttribute("member") instanceof Member member) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null
+                && session.getAttribute("member") != null
+                && session.getAttribute("member") instanceof Member member
+        ) {
             memberId = member.getId();
         }
 
         long elapsedTime = -1;
-        if (request.getAttribute("req_start_time") != null && request.getAttribute("req_start_time") instanceof Instant start) {
+        if (request.getAttribute("req_start_time") != null
+                && request.getAttribute("req_start_time") instanceof Instant start
+        ) {
             elapsedTime = Instant.now().toEpochMilli() - start.toEpochMilli();
         }
 
         String msg = "-";
         if (ex != null) {
             msg = ex.getMessage();
-        } else if (request.getAttribute("exception_msg") != null && request.getAttribute("exception_msg") instanceof String exceptionMsg) {
+        } else if (request.getAttribute("exception_msg") != null
+                && request.getAttribute("exception_msg") instanceof String exceptionMsg
+        ) {
             msg = exceptionMsg;
         }
 
